5a672f9b9f61ea38473d6ae660c280b1e8a2e900
YuYoHan <zxzz8014@naver.com>
2024-05-28 22:05:50 +0900

    [Study] JS 01 : 변수

---
+++ js/self_study/ex01/변수.js
@@ -0,0 +1,16 @@
+let num1 = 10;
+let num2 = 11.11;
+let num3 = "20";
+
+let str1 = "문자열"
+let str2 = '문자열'
+// ~ 키에 있는 따옴표 : `(백쿼트)
+let str3 = `문자열
+입니다`
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(str1);
+console.log(str2);
+console.log(str3);
